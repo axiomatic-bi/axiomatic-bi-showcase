2e07534b8ae914213cab5ae3e2978957925ff09c
amisont <134074972+amisont@users.noreply.github.com>
2026-04-23 16:22:44 +0100

    Update Astro configuration with site URL and base path. Enhance BaseLayout styles by introducing new CSS variables for max-width settings, improving layout consistency and responsiveness across components.

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import react from "@astrojs/react";
 
 export default defineConfig({
   output: "static",
+  site: "https://axiomatic-bi.github.io",
+  base: "/axiomatic-bi-showcase",
   integrations: [mdx(), react()]
 });
